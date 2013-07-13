1ccf9d884f52f5db03a57d2f97ea54ff873e136a
mwuhahaha <dylan.einhorn@gmail.com>
2013-07-13 22:52:06 +0000

    testing readFile

--- web.js
+++ web.js
@@ -2,10 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var i = fs.readFileSync("index.html");
+var content;
+
+var i = fs.readFileSync("./index.html", function read(err,data){
+ if(err){
+  throw err;
+ }
+ content = data;
+});
 
 app.get('/', function(request, response) {
-  response.send(i);  // echo out index.html
+  response.send(content);  // echo out index.html
 });
 
 var port = process.env.PORT || 5000;
